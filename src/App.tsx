3e1b350bcc2e5f578c7fe422bfd40d40e8db020a
SasipasCh <sasipas.chi@lightnet.io>
2023-11-27 11:57:34 +0700

    feat: example translation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useAtom } from 'jotai';
 import { userAtom } from './container/stores/demo/DemoStore';
 import PrimaryButton from './components/button/PrimaryButton';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function App() {
   const [user] = useAtom(userAtom);
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleClick = () => {
     navigate('/demo');
@@ -14,7 +16,7 @@ function App() {
   return (
     <div>
       <p>
-        User: {user.name}, Age: {user.age}
+        {t('test')}: {user.name}, Age: {user.age}
       </p>
       <PrimaryButton title="Demo" onClick={() => handleClick()} />
     </div>
